965aa402385bb229213a5db12264442102632a6e
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2024-04-27 07:42:49 +0100

    register user to admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from user.models import User
+
+
+@admin.register(User)
 # Register your models here.
+class FastWalletUser(UserAdmin):
+    pass
--- user/serializer.py
+++ user/serializer.py
@@ -8,13 +8,17 @@ from user.models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=8, max_length=16, write_only=True)
+    wallet_pin = serializers.CharField(min_length=4, max_length=4, write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'wallet_pin']
 
     def create(self, validated_data):
-        pin = validated_data.g
-        user = User.objects.create_user(**validated_data)
-        E_Wallet.objects.create(user_id=user.pk)
+        pin = validated_data.get('wallet_pin')
+        username = validated_data.get('username')
+        email = validated_data.get('email')
+        password = validated_data.get('password')
+        user = User.objects.create_user(username=username, email=email, password=password)
+        E_Wallet.objects.create(user_id=user.pk, pin=pin)
         return user
